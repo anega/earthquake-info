b965e67bbd03fc7a3267ebfe6eccb502cc8a41fd
anega <anegamega@gmail.com>
2017-11-20 13:47:51 +0200

    Sort data settings functionslity

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.quakereport;
 
 import android.content.SharedPreferences;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
@@ -24,6 +25,26 @@ public class SettingsActivity extends AppCompatActivity {
 
             Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
             bindPreferenceSummaryToValue(minMagnitude);
+
+            Preference orderBy = findPreference(getString(R.string.settings_order_by_key));
+            bindPreferenceSummaryToValue(orderBy);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object o) {
+            String stringValue = o.toString();
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int prefIndex = listPreference.findIndexOfValue(stringValue);
+                if (prefIndex >= 0) {
+                    CharSequence[] labels = listPreference.getEntries();
+                    preference.setSummary(labels[prefIndex]);
+                }
+            } else {
+                preference.setSummary(stringValue);
+            }
+
+            return true;
         }
 
         private void bindPreferenceSummaryToValue(Preference preference) {
@@ -31,13 +52,6 @@ public class SettingsActivity extends AppCompatActivity {
             SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
             String preferenceString = preferences.getString(preference.getKey(), "");
             onPreferenceChange(preference, preferenceString);
-        }
-
-        @Override
-        public boolean onPreferenceChange(Preference preference, Object o) {
-            String stringValue = o.toString();
-            preference.setSummary(stringValue);
-            return true;
         }
     }
 }
